540b22936bfa9669a04aa3630b1cdc840ad2ed2a
Kunal Roy <kunalroy@berkeley.edu>
2013-11-09 12:41:00 -0800

    Should not include hidden file directories

--- recentfiles.py
+++ recentfiles.py
@@ -18,18 +18,19 @@ def unsorted_list(PATH, Days): ## returns unsorted list of recent files accessed
 	min_mtime = datetime.datetime.today()  ## sets the last date you do not want to include 
 	difference=datetime.timedelta(days=Days)
 	min_mtime=min_mtime-difference # sets the earliest date from which the files should be
-	recent_files=['git'] ## the list that will return in the form [(fullpath name,(date,filesize))]
-	not_include= #creates file types that we don't want to include 
+	recent_files=[] ## the list that will return in the form [(fullpath name,(date,filesize))]
+	not_include= ['.git']#creates file types that we don't want to include 
 	for dirname,subdirs,files in os.walk(PATH):
-	    for fname in files:
-	        full_path = os.path.join(dirname, fname)
-	        mtime = os.stat(full_path).st_mtime
-	        mtime=datetime.datetime.fromtimestamp(mtime)
-	        templist=fullpath.split(".")
-	        filetype=templist[-1]
-	        if mtime > min_mtime:
-	        	if filetype not in not_include:
-	            	recent_files.append(CacheFile(full_path,mtime,os.path.getsize(full_path))) ## appends to the recent files list
+		files = [f for f in files if not f[0] == '.']
+		for fname in files:
+			full_path = os.path.join(dirname, fname)
+			mtime = os.stat(full_path).st_mtime
+			mtime=datetime.datetime.fromtimestamp(mtime)
+			templist=full_path.split(".")
+			filetype=templist[-1]
+			if mtime > min_mtime:
+				if filetype not in not_include:
+					recent_files.append(CacheFile(full_path,mtime,os.path.getsize(full_path))) ## appends to the recent files list
 	return recent_files
-print(unsorted_list())
+print(unsorted_list("C:/Users/Kunal/Documents",7))
 
