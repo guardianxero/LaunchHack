6f4d7d63df08e78d346906554e3d9a2f281b29aa
guardianxero <guardianxero@gmail.com>
2013-11-09 11:06:43 -0800

    Changing files

--- cachefile.py
+++
@@ -1,9 +0,0 @@
-class CacheFile:
-	def __init__(self,directory,date,size):
-		self.directory = directory
-		temp = self.directory.split('/')
-		self.name = temp[-1]
-		self.date = date
-		self.size = size
-
-	
